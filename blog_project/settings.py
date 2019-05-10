72ce1829d1236da9a76159e3377d7d61a2a516a0
Ralph Flick <rwflick@gmail.com>
2019-05-10 13:26:42 -0700

    heroku config further update to whitenoise

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
-    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
